23b2f32a1ef61ab9deff8f2b6e9c9083565613e8
MatisseTSP <jeanmariematisse@gmail.com>
2025-05-07 13:27:58 +0100

    test

--- dev/notreProjet/src/test/java/models/GameTest.java
+++ dev/notreProjet/src/test/java/models/GameTest.java
@@ -1,6 +1,7 @@
 package models;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.fail;
 
 public class GameTest {
     @Test
@@ -10,14 +11,25 @@ public class GameTest {
 
     @Test
     void testCheckCollision() {
-    height = 10;
-    newX = 2;
-    newY = 9;
-    squareRatio = 2;
-    if (checkCollision(int newX, int newY) == false) {
-        fail("newY>= height - squareRatio")
+
+        // quand ca touche le bas
+        SandArea testsandarea = new SandArea(10,10,2);
+        int newX = 2;
+        int newY = 9;
+        if (testsandarea.checkCollision(int newX,int newY) == false) {
+        fail("newY>= height - squareRatio");
         }
-    
+
+        // quand il y a un block deja présent
+        // Simule un bloc présent à (4,4)
+        testsandarea.createBlock(4, 4, javafx.scene.paint.Color.RED);
+        // Essaye de poser une forme par-dessus : collision attendue
+        boolean result = testsandarea2.checkCollision(4, 3);
+        assertTrue(result, "Collision attendue avec un bloc déjà présent");
+
+        //espace suffisant
+        boolean result = testsandarea.checkCollision(1, 1);
+        assertFalse(result, "Pas de collision attendue : zone vide");
     }
 
     @Test
